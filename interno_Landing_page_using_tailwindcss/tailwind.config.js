fbd7fd891e813eea475f4129dad336b6b7200d28
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-06 00:10:25 +0530

    commit

--- interno_Landing_page_using_tailwindcss/tailwind.config.js
+++ interno_Landing_page_using_tailwindcss/tailwind.config.js
@@ -5,13 +5,33 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
 
-
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1280px",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         noto: ["Noto Serif Display", "serif"],
       },
+      backgroundImage: {
+        hero: "url(./assets/alberto-castillo-q-mx4mSkK9zeo-unsplash.jpg)"
+      },
+
+      colors: {
+        main: "#C2FBEF",
+        secend: "#80FFEC",
+        heading_Color: "#8E4A49",
+        text_color: "#191919",
+      }
     },
   },
   plugins: [],
