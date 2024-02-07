3bc03c0c8164266467918cb1704f26720086bb54
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-07 16:33:37 +0530

    commit

--- interno_Landing_page_using_tailwindcss/main.js
+++ interno_Landing_page_using_tailwindcss/main.js
@@ -6,3 +6,14 @@ let navMenu = document.getElementById("nav-menu");
 navBtn.addEventListener("click", function () {
     navMenu.classList.toggle("menuOpen");
 })
+
+
+// swiper 
+
+let swiper = new swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+})
