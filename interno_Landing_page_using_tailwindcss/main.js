8ca48fd00378b648d472f5485b2fe6071f0e3439
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-08 21:29:48 +0530

    commit

--- interno_Landing_page_using_tailwindcss/main.js
+++ interno_Landing_page_using_tailwindcss/main.js
@@ -10,10 +10,42 @@ navBtn.addEventListener("click", function () {
 
 // swiper 
 
-let swiper = new swiper(".swiper", {
+let swiper = new Swiper(".swiper", {
     loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+
+    autoplay: {
+        delay: 2000,
+    },
+
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        960: {
+            slidesPerView: 3,
+        },
+
     }
 })
+
+// scroll reveal 
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+});
+
+
+sr.reveal('.hero__text', { origin: "top" })
+
