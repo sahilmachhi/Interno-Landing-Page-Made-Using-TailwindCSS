4416857795cf920435d2f953728d47361d33b7f4
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-09 22:03:11 +0530

    commit

--- interno_Landing_page_using_tailwindcss/main.js
+++ interno_Landing_page_using_tailwindcss/main.js
@@ -56,6 +56,10 @@ sr.reveal('.steps__step', { origin: "top", interval: 300, });
 sr.reveal('.about__text', { origin: "right", });
 sr.reveal('.about__img', { origin: "left", });
 
+// testimonial 
+sr.reveal('.testimonial-bg');
+sr.reveal('.testimonial-bg .h2');
+sr.reveal('.testimonial__slider .swiper-slide', { interval: 300, });
 
 // brands 
 sr.reveal('.brands__img img', { origin: "top", interval: 150, });
@@ -65,3 +69,21 @@ sr.reveal('.brands__img img', { origin: "top", interval: 150, });
 sr.reveal('.work__heading h2,.work__heading p', { origin: "top", interval: 500, });
 sr.reveal('.work__item__left', { origin: "right", });
 sr.reveal('.work__item__right', { origin: "left", });
+
+
+// states 
+sr.reveal('.states');
+sr.reveal('.states__grid div', { interval: 150, });
+
+// news 
+sr.reveal('.news');
+sr.reveal('.news__text h2,.news__text p', { interval: 150, });
+sr.reveal('.news__grid div', { origin: "top", interval: 200, });
+
+// contact 
+sr.reveal('.contact');
+sr.reveal('.contact__text h2,.contact__text p,.contact__text button', { interval: 150, });
+
+// footer 
+sr.reveal('.footer');
+sr.reveal('.footer__item', { interval: 150, });
